b30ac611bff08b90152370a8d7a4d251f0007eb9
eiznem <eiznem@users.noreply.github.com>
2024-12-23 16:19:02 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,38 @@
+const CACHE_NAME = 'black-cards-v2';
+const CACHE_ASSETS = [
+    './index.html',
+    './manifest.json',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Cards_Against_Humanity_logo.png/512px-Cards_Against_Humanity_logo.png'
+];
+
+// Install Event
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('black-cards-cache').then((cache) => {
-            return cache.addAll([
-                './index.html',
-                './manifest.json',
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Cards_Against_Humanity_logo.png/512px-Cards_Against_Humanity_logo.png'
-            ]);
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(CACHE_ASSETS);
         })
     );
-    console.log('Service Worker installed.');
+    console.log('Service Worker: Installed');
 });
 
+// Activate Event
 self.addEventListener('activate', (event) => {
-    console.log('Service Worker activated.');
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Service Worker: Clearing Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    console.log('Service Worker: Activated');
 });
 
+// Fetch Event
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
